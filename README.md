d27edb4610b11ddd132ee7e6132664a16381a89f
ssmith2006 <shantel.smith@hotmail.com>
2025-04-21 01:01:25 +0000

    installed lodash and ran a start script with no errors

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # node-review
+
+![alt text](image.png)
+![alt text](image-1.png)
+![alt text](image-2.png)
--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 "use strict";
 
+import _ from "lodash";
 import { multiplyThreeNumbers, greet } from "./util.js";
 
 console.log (multiplyThreeNumbers(3, 3, 3));
 console.log (greet("My name is Shantel!"));
+console.log(`Hello, ${greet("Shantel")}!`);
