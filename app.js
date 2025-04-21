740aacedcc96b75d60351009f77e5580084ff37b
ssmith2006 <shantel.smith@hotmail.com>
2025-04-21 00:28:26 +0000

    imported and exported modules

---
+++ app.js
@@ -0,0 +1,6 @@
+"use strict";
+
+import { multiplyThreeNumbers, greet } from "./util.js";
+
+console.log (multiplyThreeNumbers(3, 3, 3));
+console.log (greet("My name is Shantel!"));
---
+++ util.js
@@ -0,0 +1,9 @@
+"use strict";
+
+export function greet (name){
+    return (name);
+}
+
+export function multiplyThreeNumbers(num1, num2, num3){
+    return (num1, num2, num3);
+}
